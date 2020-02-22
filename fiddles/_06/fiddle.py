8b30963284557d9f6f6facd19ea4cfb9af1302fc
Gulácsy-Tóth Tamás <tamas.gulacsy.toth@lensa.com>
2020-02-22 14:00:45 +0100

    fiddle 06 part 2

--- fiddles/_06/fiddle.py
+++ fiddles/_06/fiddle.py
@@ -1,8 +1,8 @@
 import csv
 
-def build_orbit_parents(input):
+def build_orbit_parents(map):
     orbit_parents = {}
-    for [parent, obj] in input:
+    for [parent, obj] in map:
         orbit_parents[obj] = parent
     return orbit_parents
 
@@ -12,15 +12,45 @@ def count_parents(orbit_parents, orbit):
         return 0
     return 1 + count_parents(orbit_parents, parent)
 
-def get_checksum(input):
-    orbit_parents = build_orbit_parents(input)
+def get_checksum(map):
+    orbit_parents = build_orbit_parents(map)
     checksum = 0
     for orbit in orbit_parents:
         checksum = checksum + count_parents(orbit_parents, orbit)
     return checksum
 
+def get_parents(orbit_parents, orbit):
+    parent = orbit_parents.get(orbit, None)
+    if parent == None:
+        return []
+    return [parent] + get_parents(orbit_parents, parent)
+
+def get_first_common_parent(orbit_parents, orb1, orb2):
+    orb1_parents = get_parents(orbit_parents, orb1)
+    orb2_parents = get_parents(orbit_parents, orb2)
+    common_parents = [
+        orb1_parent for orb1_parent in orb1_parents if orb1_parent in orb2_parents
+    ]
+    return common_parents[0]
+
+def count_parents_to(orbit_parents, orbit, to):
+    parent = orbit_parents.get(orbit, None)
+    if  parent == None:
+        raise ValueError(f'{orbit} does not have {to} parent')
+    if parent == to:
+        return 0
+    return 1 + count_parents_to(orbit_parents, parent, to)
+
+def count_min_transfers(map, orb1, orb2):
+    orbit_parents = build_orbit_parents(map)
+    first_common_parent = get_first_common_parent(orbit_parents, orb1, orb2)
+    return (
+        count_parents_to(orbit_parents, orb1, first_common_parent) +
+        count_parents_to(orbit_parents, orb2, first_common_parent)
+    )
 
 if __name__ == '__main__':
     with open('./input.txt') as fiddle_input:
-        map = [input for input in csv.reader(fiddle_input, delimiter=')')]
+        map = [orbit_relation for orbit_relation in csv.reader(fiddle_input, delimiter=')')]
         print(get_checksum(map))
+        print(count_min_transfers(map, 'YOU', 'SAN'))
--- fiddles/_06/test_fiddle.py
+++ fiddles/_06/test_fiddle.py
@@ -3,24 +3,27 @@ from fiddles._06.fiddle import (
     build_orbit_parents,
     count_parents,
     get_checksum,
+    get_parents,
+    get_first_common_parent,
+    count_parents_to,
+    count_min_transfers,
 )
 
 
 class TestFiddle(unittest.TestCase):
     def test_build_orbit_parents(self):
-        input = [
+        map = [
             ['A', 'B'],
             ['COM', 'A'],
             ['B', 'C']
         ]
         self.assertEqual(
-            build_orbit_parents(input),
+            build_orbit_parents(map),
             {
                 'A': 'COM',
                 'B': 'A',
                 'C': 'B'
             },
-            'Building orbit parents'
         )
 
     def test_count_parents(self):
@@ -40,7 +43,7 @@ class TestFiddle(unittest.TestCase):
         )
 
     def test_get_checksum(self):
-        input = [
+        map = [
             ['COM', 'B'],
             ['B', 'C'],
             ['C', 'D'],
@@ -55,8 +58,85 @@ class TestFiddle(unittest.TestCase):
         ]
 
         self.assertEqual(
-            get_checksum(input),
+            get_checksum(map),
             42
+        )
+
+    def test_get_parents(self):
+        orbit_parents = {
+            'A': 'COM',
+            'B': 'A',
+            'C': 'B'
+        }
+
+        self.assertEqual(
+            get_parents(orbit_parents, 'C'),
+            ['B', 'A', 'COM']
+        )
+
+    def test_get_first_common_parent(self):
+        map = [
+            ['COM', 'B'],
+            ['B', 'C'],
+            ['C', 'D'],
+            ['D', 'E'],
+            ['E', 'F'],
+            ['B', 'G'],
+            ['G', 'H'],
+            ['D', 'I'],
+            ['E', 'J'],
+            ['J', 'K'],
+            ['K', 'L'],
+            ['K', 'YOU'],
+            ['I', 'SAN'],
+        ]
+
+        self.assertEqual(
+            get_first_common_parent(build_orbit_parents(map), 'YOU', 'SAN'),
+            'D'
+        )
+
+    def test_count_parents_to(self):
+        map = [
+            ['COM', 'B'],
+            ['B', 'C'],
+            ['C', 'D'],
+            ['D', 'E'],
+            ['E', 'F'],
+            ['B', 'G'],
+            ['G', 'H'],
+            ['D', 'I'],
+            ['E', 'J'],
+            ['J', 'K'],
+            ['K', 'L'],
+            ['K', 'YOU'],
+            ['I', 'SAN'],
+        ]
+
+        self.assertEqual(
+            count_parents_to(build_orbit_parents(map), 'YOU', 'D'),
+            3
+        )
+
+    def test_count_min_transfers(self):
+        map = [
+            ['COM', 'B'],
+            ['B', 'C'],
+            ['C', 'D'],
+            ['D', 'E'],
+            ['E', 'F'],
+            ['B', 'G'],
+            ['G', 'H'],
+            ['D', 'I'],
+            ['E', 'J'],
+            ['J', 'K'],
+            ['K', 'L'],
+            ['K', 'YOU'],
+            ['I', 'SAN'],
+        ]
+        self.assertEqual(
+            count_min_transfers(map, 'YOU', 'SAN'),
+            4
         )
 
 if __name__ == '__main__':
